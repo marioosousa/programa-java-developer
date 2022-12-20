b811120fb4aa6dc95e70216109132b8e59d9bc45
marioosousa <121065562+marioosousa@users.noreply.github.com>
2022-12-20 09:20:54 -0300

    valores boleanos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -13,5 +13,33 @@ public class Operadores {
         System.out.println(concatenacao);
         concatenacao = "1"+(1+1+1);
         System.out.println(concatenacao);
+        int numero = 5;
+        numero = -numero;
+        System.out.println(numero);
+        numero = numero * -1;
+        System.out.println(numero);
+        // incremento
+        numero = numero + 1;
+        System.out.print("Incremento: ");
+        System.out.println(numero);
+        // Decrementar
+        numero = numero - 1;
+        System.out.print("Decremento ( numero = numero - 1): ");
+        System.out.println(numero);
+        numero = -- numero;
+        System.out.print("Decremento ( numero = -- numero): ");
+        System.out.println(numero);
+        System.out.print("Decremento ( System.out.println(--numero)): ");
+        System.out.println(--numero);
+
+        //Boleanos
+        boolean variavel = true;
+        System.out.println("Valores Boleanos");
+        System.out.println("Variável boleana recebe '!' muda o valor");
+        System.out.println("booLean variavel = true");
+        System.out.println("System.out.println(!variavel)");
+        System.out.print("Variável: ");
+        System.out.println(!variavel);
+
     }
 }
