4f1baa2ecd721456ef52f0f1f32d1b54980af224
marioosousa <121065562+marioosousa@users.noreply.github.com>
2022-12-27 19:32:11 -0300

    conta banco

---
+++ cauculadora-javadoc/src/Cauculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Cauculador</h1>
+ * <p>A cauculadora realiza operações matemáticas entre números inteiros</p>
+ * <p><b>Note:</b> Leia atentamente a documentação desta classes para desfrutar
+ * dos recursos oferecidos pelo autor</p>
+ * 
+ * @author Mario Henrique
+ * @version 1.0
+ * @since 21/12/2022  
+ */
+
+public class Cauculadora{
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma de dois números. 
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+} 
+
