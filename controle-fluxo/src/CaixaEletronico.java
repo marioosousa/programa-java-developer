f8328f52903050ac6265e0155e50838ed2222670
marioosousa <121065562+marioosousa@users.noreply.github.com>
2022-12-28 20:17:34 -0300

    Update CaixaEletronico.java

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,16 +1,12 @@
-import javax.lang.model.util.ElementScanner6;
-
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         double saldo = 25.0;
         double valorSolicitado = 25.0;
 
-        if(valorSolicitado <= saldo){
+        if(valorSolicitado <= saldo)
             saldo = saldo -valorSolicitado;
-            System.out.println(saldo);
-        } 
-        else if(valorSolicitado > saldo){
+        else
             System.out.println("Saldo insuficiente!");
-        }
+        System.out.println("Saldo: R$"+saldo);
     }
 }
