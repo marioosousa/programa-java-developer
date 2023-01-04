8fa9978a7c7cb55795120a9f454c5b3887a24f07
marioosousa <121065562+marioosousa@users.noreply.github.com>
2023-01-04 19:11:53 -0300

    Create ExemploExceção
    Hierarchy of exceptions and custom exceptions

---
+++ controle-fluxo/src/ExemploExcecao.java
@@ -0,0 +1,15 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+public class ExemploExcecao {
+    public static void main(String[] args) {
+
+        Number number;
+        try {
+            number = NumberFormat.getInstance().parse("a1.75");
+            System.out.println(number);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
