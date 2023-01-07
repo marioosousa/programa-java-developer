a67a5c6ebfe64622ff746147a9d5e44e13228f86
marioosousa <121065562+marioosousa@users.noreply.github.com>
2023-01-07 13:31:28 -0300

    Update FormatadorCepExemplo.java

--- controle-fluxo/src/FormatadorCepExemplo.java
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -1,11 +1,17 @@
 public class FormatadorCepExemplo{
     public static void main(String[] args) {
+        try {
+            String cepFormatado = formartarCep("53030060");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("o número CEP está invalido");
+        }
     }
     static String formartarCep(String cep) throws CepInvalidoException{
-        if(cep.length()!8)
+        if(cep.length()!=8)
             throw new CepInvalidoException();
 
             //Simulando formatação
-            return "23.765-064"
+            return "23.765-064";
     }
 }
