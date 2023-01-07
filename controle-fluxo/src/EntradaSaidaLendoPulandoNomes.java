83f44b7867c489f50ffc711f2ea494f250bcd5db
marioosousa <121065562+marioosousa@users.noreply.github.com>
2023-01-07 13:31:21 -0300

    Create EntradaSaidaLendoPulandoNomes.java

---
+++ controle-fluxo/src/EntradaSaidaLendoPulandoNomes.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class EntradaSaidaLendoPulandoNomes {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String nomes[];
+		
+		for(int i=0 ; i<10 ; i++)
+		    nomes[1] = sc.next();
+        System.out.println(nomes[3]);
+	}
+    
+}
